f9878eced464511fac2df85ac8029e9a5ddc1367
chankoma <komaki.yasumaki@gmail.com>
2025-02-28 14:07:39 +0900

    sitemap-v2

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,5 @@
 import { MetadataRoute } from "next";
-import { getAllCategoryList, getAllNewsList, News } from "./libs/microcms";
-import { MicroCMSListResponse } from "microcms-js-sdk";
+import { getAllNewsList, News } from "./libs/microcms";
 
 const buildUrl = (path?: string) =>
   `https://chankoma-next-blog.vercel.app${path ?? ""}`;
@@ -8,19 +7,10 @@ const buildUrl = (path?: string) =>
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const newsContents: any = await getAllNewsList();
 
-  const categoryContents: any = await getAllCategoryList();
-
   const newsUrls: MetadataRoute.Sitemap = newsContents.map((content: News) => ({
     url: buildUrl(`/news/${content.id}`),
     lastModified: content.revisedAt,
   }));
-
-  const categoryUrls: MetadataRoute.Sitemap = categoryContents.map(
-    (content: News) => ({
-      url: buildUrl(`/news/category/${content.id}`),
-      lastModified: content.revisedAt,
-    })
-  );
 
   const now = new Date();
 
@@ -31,10 +21,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     },
     {
       url: buildUrl("/members"),
-      lastModified: now,
-    },
-    {
-      url: buildUrl("/contact"),
       lastModified: now,
     },
     {
